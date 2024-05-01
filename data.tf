f1f6d3e1c50499d61846004a3c9e20d9a78d1c62
sandeep <munagalasandeepreddy@gamail.com>
2024-05-01 11:25:08 +0200

    learn terraform

--- data.tf
+++
@@ -1,5 +0,0 @@
-#data "aws_ami" "ami" {
- # owners = ["973714476881"]
-  #most_recent = true
-  #name_regex = "Centos-8-DevOps-Practice"
-#}
--- main.tf
+++
@@ -1,5 +0,0 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo Hello world - ${var.env}"
-  }
-}
