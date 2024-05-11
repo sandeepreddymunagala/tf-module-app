0b15575b1d7b143d48a1d31381d3368a75965200
sandeep <munagalasandeepreddy@gamail.com>
2024-05-11 10:38:36 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -135,6 +135,7 @@ resource "null_resource" "ansible" {
 resource "aws_security_group" "sg" {
   name        = "${var.component}-${var.env}-sg"
   description = "${var.component}-${var.env}-sg"
+  vpc_id = var.vpc_id
 
   ingress {
     from_port   = 0
