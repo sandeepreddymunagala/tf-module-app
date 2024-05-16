0e01291fb5fec83c7e71bc331297860b9f8300b0
sandeep <munagalasandeepreddy@gamail.com>
2024-05-16 18:26:52 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -1,79 +1,33 @@
-/*## policy
-resource "aws_iam_policy" "policy" {
-  name        = "${var.component}-${var.env}-ssm-pm-policy"
-  path        = "/"
-  description = "${var.component}-${var.env}-ssm-pm-policy"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "VisualEditor0",
-        "Effect": "Allow",
-        "Action": [
-          "ssm:GetParameterHistory",
-          "ssm:GetParametersByPath",
-          "ssm:GetParameters",
-          "ssm:GetParameter"
-        ],
-        "Resource": [
-          "arn:aws:ssm:us-east-1:339712793158:parameter/roboshop.${var.env}.${var.component}.*",
-        ]
-      }
-    ]
-  })
-}
-
-## iam role
-resource "aws_iam_role" "role" {
-  name = "${var.component}-${var.env}-ec2-role"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  tags = {
-    tag-key = "tag-value"
-  }
-}
-
-resource "aws_iam_instance_profile" "instance_profile" {
-  name = "${var.component}-${var.env}-ec2-role"
-  role = aws_iam_role.role.name
-}
-resource "aws_iam_role_policy_attachment" "policy-attach" {
-  role        = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.policy.arn
-}
-## security group
-resource "aws_security_group" "sg" {
-  name = "${var.component}-${var.env}-sg"
+resource "aws_security_group" "main" {
+  name        = "${var.component}-${var.env}-sg"
   description = "${var.component}-${var.env}-sg"
+  vpc_id      = var.vpc_id
 
   ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = var.sg_subnets_cidr
   }
+
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = var.allow_prometheus_cidr
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allow_ssh_cidr
+  }
+
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -81,88 +35,59 @@ resource "aws_security_group" "sg" {
     Name = "${var.component}-${var.env}-sg"
   }
 }
-## Ec2
-resource "aws_instance" "instance" {
-  ami           = data.aws_ami.ami.id
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
-  tags = merge({
-    Name = "${var.component}-${var.env}"
-  },var.tags)
+resource "aws_launch_template" "main" {
+  name = "${var.component}-${var.env}"
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.instance_profile.name
+  }
+  image_id               = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags          = merge({ Name = "${var.component}-${var.env}", Monitor = "true" }, var.tags)
+  }
+
+  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
+    env       = var.env
+    component = var.component
+  }))
+
+  block_device_mappings {
+  device_name = "/dev/sda1"
+
+  ebs {
+  volume_size = 10
+  encrypted   = "true"
+  kms_key_id  = var.kms_key_id
+  }
+  }
+}
+
+resource "aws_autoscaling_group" "main" {
+  desired_capacity = var.desired_capacity
+  max_size = var.max_size
+  min_size = var.min_size
+
+  launch_template {
+    id = aws_launch_template.main.id
+    version = "$Latest"
+  }
 }
 
 ## DNS record
-resource "aws_route53_record" "dns" {
+/*resource "aws_route53_record" "dns" {
   name    = "${var.component}-${var.env}.sandeepreddymunagala123.xyz"
   type    = "A"
   zone_id = "Z000681610YP12S51X5A5"
   ttl     = 300  # Example TTL value (in seconds)
 
-  records = [
-    aws_instance.instance.private_ip
-  ]
+  records = [var.lb_dns_name]
 }
-
-##Null resource-ansible
-resource "null_resource" "ansible" {
-  depends_on = [
-    aws_instance.instance,
-    aws_route53_record.dns
-  ]
-
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"  # Use SSH key-based authentication instead
-      host     = aws_instance.instance.public_ip
-    }
-
-    // Use more detailed logging and error handling
-    inline = [
-      "echo 'Starting Ansible execution'",
-      "sudo labauto ansible",
-      "ansible-pull -i localhost, -U https://github.com/sandeepreddymunagala/roboshop-ansible main.yml -e env=${var.env} -e role_name=${var.component} || echo 'Ansible execution failed with exit code: $?'"
-    ]
-  }
-}
-
 */
 
 
-resource "aws_security_group" "sg" {
-  name        = "${var.component}-${var.env}-sg"
-  description = "${var.component}-${var.env}-sg"
-  vpc_id = var.vpc_id
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.component}-${var.env}-sg"
-  }
-}
-
-resource "aws_instance" "test" {
-  ami           = data.aws_ami.ami.id
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = var.subnet_id
-
-  tags = {
-    Name = var.component
-  }
-}
 
--- variables.tf
+++ variables.tf
@@ -3,5 +3,19 @@ variable "component" {}
 variable "tags" {
   default = {}
 }
-variable "subnet_id" {}
+variable "subnets" {}
 variable "vpc_id" {}
+variable "sg_subnets_cidr" {}
+variable "app_port" {}
+variable "kms_key_id" {}
+variable "instance_type" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "allow_ssh_cidr" {}
+variable "lb_dns_name" {}
+variable "listener_arn" {}
+variable "lb_rule_priority" {}
+variable "kms_arn" {}
+variable "extra_param_access" {}
+variable "allow_prometheus_cidr" {}
