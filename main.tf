8a5a80fca510e61ed7c8f8d322d25ac50fc24547
sandeep <munagalasandeepreddy@gamail.com>
2024-05-17 09:39:16 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -79,6 +79,8 @@ resource "aws_autoscaling_group" "main" {
   desired_capacity = var.desired_capacity
   max_size = var.max_size
   min_size = var.min_size
+  vpc_zone_identifier = var.subnets
+  target_group_arns   = [aws_lb_target_group.main.arn]
   launch_template {
     id = aws_launch_template.main.id
     version = "$Latest"
