b1affccaa47aea1d8cd21d6c07148042c5baa332
sandeep <munagalasandeepreddy@gamail.com>
2024-05-17 09:26:59 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -46,14 +46,14 @@ resource "aws_launch_template" "main" {
     component = var.component
   }))
 
-  block_device_mappings {
+/*  block_device_mappings {
   device_name = "/dev/sda1"
   ebs {
   volume_size = 10
   encrypted   = "true"
   kms_key_id  = var.kms_key_id
   }
- }
+ }*/
 }
 ## Ec2
 resource "aws_autoscaling_group" "main" {
